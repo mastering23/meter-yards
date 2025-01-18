64ef0c09c1796bfb6517f0e435c79c2c6f9fc5e1
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-01-17 22:53:01 -0500

    adding a feature for random yards and also user can add its own yards

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
-const convertToMeters = () => {  
+const convertToMeters = () => {
+  const inputOpt = prompt(`
+    Option :
+    ------------------------
+    Type 1 for a random number of yards
+    Type 2 to enter your own value
+  `);
 
-  const inputYards = prompt(`Enter yeards units : `);
+  const selectOpt = Number(inputOpt);
 
-  const yards = Number(inputYards);
+  if (selectOpt === 1) {
 
-  const meter = yards * 0.9144;
+    let randomYard = Math.floor(Math.random() * 1000);
 
-  const finalMessage = `from ${yards} convert to ${meter} meters `;
+    const meter = Math.floor(randomYard * 0.9144);
 
-  const finalMessage2 = createMessage(yards);
+    const finalMessage = `from ${randomYard} convert to ${meter} meters `;
 
-  alert(`${finalMessage} ${finalMessage2} `);
+    const finalMessage2 = createMessage(randomYard);
+
+    alert(`${finalMessage} ${finalMessage2} `);
+
+  } else if (selectOpt === 2) {
+
+    const inputYards = prompt(`Enter yeards units : `);
+
+    yards = Number(inputYards);
+
+    const meter = yards * 0.9144;
+
+    const finalMessage = `from ${yards} convert to ${meter} meters `;
+    
+    const finalMessage2 = createMessage(yards);
+
+    alert(`${finalMessage} ${finalMessage2} `);
+
+
+  } else {
+    
+    return convertToMeters();
+  }
 };
 
 const createMessage = (yards) => {
+  
   let message = ``;
 
   const numYards = yards;
